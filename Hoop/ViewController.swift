cdddd15981f8f3265d2d874d1f42d9b99d03e8d3
James Oeth <oeth@usc.edu>
2018-03-18 12:35:05 -0700

    Initial Commit

---
+++ Hoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hoop
+//
+//  Created by james oeth on 3/18/18.
+//  Copyright © 2018 jamesOeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
